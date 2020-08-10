a01d5b4313746268689cfff6987f2a38206d3d12
sciaravino <59625798+sciaravino@users.noreply.github.com>
2020-08-10 15:19:37 -0400

    added local storage

--- appes6.js
+++ appes6.js
@@ -58,6 +58,49 @@ class UI {
     }
 };
 
+// Local Storage Class
+class Store{
+    static getBooks(){
+        let books;
+        if(localStorage.getItem('books') === null){
+            books = [];
+        }else{
+            books = JSON.parse(localStorage.getItem('books'));
+        }
+
+        return books;
+    }
+
+    static displayBooks(){
+        const books = Store.getBooks();
+        books.forEach(function(book){
+            const ui = new UI;
+            // add book to UI
+            ui.addBookToList(book);
+        })
+    }
+
+    static addBook(book){
+        const books = Store.getBooks();
+        books.push(book);
+
+        localStorage.setItem('books', JSON.stringify(books));
+    }
+
+    static removeBook(isbn){
+        const books = Store.getBooks();
+        books.forEach(function(book, index){
+            if(book.isbn === isbn){
+                books.splice(index, 1);
+            }
+        });
+
+        localStorage.setItem('books', JSON.stringify(books));
+    }
+}
+
+// DOM Load Event
+document.addEventListener('DOMContentLoaded', Store.displayBooks);
 
 // Event Listeners
 document.getElementById('book-form').addEventListener('submit', function(e){
@@ -80,6 +123,10 @@ document.getElementById('book-form').addEventListener('submit', function(e){
         // Add book to list
         ui.addBookToList(book);
 
+        // add to Local Storage
+        Store.addBook(book);
+
+        // show success
         ui.showAlert('Book Added', 'success');
 
         // Clear fields
@@ -95,8 +142,12 @@ document.getElementById('book-form').addEventListener('submit', function(e){
 document.getElementById('book-list').addEventListener('click', function(e){
     // Instantiate UI
     const ui = new UI()
-
+    
+    // Delete Book
     ui.deleteBook(e.target);
+
+    // Remove from LS
+    Store.removeBook(e.target.parentElement.previousElementSibling.textContent);
 
     // Show message
     ui.showAlert('Book removed', 'success');
